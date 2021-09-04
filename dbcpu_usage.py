0499da999c7ca6952277c330d19dc05603972ca2
Borges <daniel.borges@dxc.com>
2021-09-04 17:27:11 -0300

    .\dbcpu_usage.py

--- dbcpu_usage.py
+++ dbcpu_usage.py
@@ -4,13 +4,19 @@ import matplotlib.pyplot as plt
 
 excel_file1 = 'C:\DBA\Python\estudos\/arquivos\/cpu_usage.xlsx'
 
-df = pd.read_excel(excel_file1, sheet_name='cpu_all',usecols='A:E')
+df = pd.read_excel(excel_file1, sheet_name='cpu_all',usecols='A:D')
 
 pivot = df.groupby(["DB_NAME","INSTANCE_NUMBER","SNAP_TIME"]).mean()
 
-#print(pivot)
+print(pivot)
+
+from matplotlib import rcParams
+
+rcParams['figure.figsize'] = 10,6
 
 plot_cpu_usage = pivot.plot(kind='line')
+
+plt.title("CPU USAGE")
 
 plt.show()
 
