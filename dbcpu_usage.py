5fe58421ff14be6713df4e1ff01ec8fe76aa3400
Borges <daniel.borges@dxc.com>
2021-09-04 13:34:50 -0300

    Project with Python
    files created by Pycharm

---
+++ dbcpu_usage.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+file_name = 'C:\DBA\Python\estudos\/arquivos\/binary.csv'
+
+df = pd.read_csv(file_name)
+
+#df.head()
+print(df)
+
+file2 = 'C:\DBA\Python\estudos\/arquivos\salarios.csv'
+
+df2 = pd.read_csv(file2)
+
+#df2.head()
+print(df2):q1
+
+
